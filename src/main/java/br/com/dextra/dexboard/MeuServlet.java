74726023fba80ae051d8a856fcf5f1f469fb7f4d
Andre Willik Valenti <awvalenti@gmail.com>
2011-09-14 15:31:42 -0300

    Primeiro commit

---
+++ src/main/java/br/com/dextra/dexboard/MeuServlet.java
@@ -0,0 +1,19 @@
+package br.com.dextra.dexboard;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MeuServlet extends HttpServlet {
+
+	private static final long serialVersionUID = -1248500946944090403L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello World");
+	}
+}
