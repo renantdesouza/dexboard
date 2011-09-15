bc566bb7ecfc0cc2a32dca9851404124f6c3e6ab
Andre Willik Valenti <awvalenti@gmail.com>
2011-09-15 09:57:19 -0300

    Buscando dados de uma planilha

--- src/main/java/br/com/dextra/dexboard/MeuServlet.java
+++ src/main/java/br/com/dextra/dexboard/MeuServlet.java
@@ -1,13 +1,16 @@
 package br.com.dextra.dexboard;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.googlecode.restitory.gae.filter.util.JsonUtil;
 
 public class MeuServlet extends HttpServlet {
 
@@ -15,12 +18,21 @@ public class MeuServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// XXX
-		int id = Integer.parseInt(req.getPathInfo().split("/")[1]);
+		String pathInfo = req.getPathInfo();
 
-		JsonElement dados = PmaDataFetcher.fetchData(id);
+		if (pathInfo.equals("/projetos")) {
 
-		resp.setContentType("application/json");
-		resp.getWriter().println(dados);
+			JsonArray dadosPlanilha = (JsonArray) PlanilhaDataFetcher.fetchData();
+
+			JsonArray jsonArray = new JsonArray();
+			for (JsonElement elemento : dadosPlanilha) {
+				int idProjeto = Integer.parseInt(elemento.getAsJsonObject().get("id").getAsString());
+				JsonElement dadosPma = PmaDataFetcher.fetchData(idProjeto);
+				jsonArray.add(dadosPma);
+			}
+
+			resp.setContentType("application/json");
+			resp.getWriter().print(jsonArray);
+		}
 	}
 }
--- src/main/java/br/com/dextra/dexboard/PmaDataFetcher.java
+++ src/main/java/br/com/dextra/dexboard/PmaDataFetcher.java
@@ -1,34 +1,11 @@
 package br.com.dextra.dexboard;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import com.google.appengine.api.urlfetch.HTTPResponse;
-import com.google.appengine.api.urlfetch.URLFetchService;
-import com.google.appengine.api.urlfetch.URLFetchServiceFactory;
 import com.google.gson.JsonElement;
-import com.googlecode.restitory.gae.filter.util.JsonUtil;
 
-public class PmaDataFetcher {
+public class PmaDataFetcher extends DataFetcher {
 
 	public static JsonElement fetchData(int idProjeto) {
-		try {
-			URLFetchService urlFetchService = URLFetchServiceFactory.getURLFetchService();
-
-			// XXX
-			String uriQuasePronta = "http://172.16.129.159:8001/services/indicadores?token=ac4ef0ec195ed24ab08d1e4a8a3a1ed0&projeto_id=";
-
-			// XXX Concatenacao insegura
-			HTTPResponse response = urlFetchService.fetch(new URL(uriQuasePronta + idProjeto));
-			String json = new String(response.getContent());
-			return JsonUtil.parse(json);
-
-		} catch (MalformedURLException e) {
-			throw new RuntimeException(e);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
+		return fetchData("http://172.16.129.159:8001/services/indicadores?token=ac4ef0ec195ed24ab08d1e4a8a3a1ed0&projeto_id=" + idProjeto);
 	}
 
 }
