98ae5f1dc3d96a2acedc756c05178b60c0fb9233
Bruno Camarini <bruno.camarini@dextra-sw.com>
2013-04-08 09:41:02 -0300

    Scrollbar contextual para container de projetos.

--- src/main/webapp/js/dexboard-rotate.js
+++ src/main/webapp/js/dexboard-rotate.js
@@ -2,20 +2,22 @@
 {
     $(document).ready(function()
     {
-        var rotate = function()
-        {
-            if ( $('#dialog').data('opened') !== true )
-            {
-                var container = $('.container-right');
-                var last = $('.container-right ul li').last();
-                var lastLiPx = last.offset().left + last.width();
-                var limitLeft = container.offset().left + container.width();
+		if ( $('body').hasClass('tv') === true ) {
+			var rotate = function()
+			{
+				if ( $('#dialog').data('opened') !== true )
+				{
+					var container = $('.container-right');
+					var last = $('.container-right ul li').last();
+					var lastLiPx = last.offset().left + last.width();
+					var limitLeft = container.offset().left + container.width();
 
-                var scrollOffset = limitLeft - lastLiPx;
+					var scrollOffset = limitLeft - lastLiPx;
 
-                container.animate({'scrollLeft' : scrollOffset > 0 ? 0 : '+=' + container.width()}, 5000, 'easeInOutCubic');
-            }
-        }
-        var rotationInterval = setInterval(rotate, 15000);
+					container.animate({'scrollLeft' : scrollOffset > 0 ? 0 : '+=' + container.width()}, 5000, 'easeInOutCubic');
+				}
+			}
+			var rotationInterval = setInterval(rotate, 15000);
+		}
     });
 })(jQuery);
