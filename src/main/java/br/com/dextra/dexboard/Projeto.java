546856a220bfcf43aade4f01479e7f5b1ffefaa4
César Borges <cesarborges@gmail.com>
2013-04-25 16:45:53 -0300

    Removendo Supress

--- src/main/java/br/com/dextra/dexboard/Projeto.java
+++ src/main/java/br/com/dextra/dexboard/Projeto.java
@@ -18,24 +18,27 @@ public class Projeto {
 
 	public final static Logger LOG = LoggerFactory.getLogger(Projeto.class);
 
-	@SuppressWarnings("unused")
 	public static final ServicoPma SERVICO_PMA_AMAZON = new ServicoPma(
-			"https://50.17.210.152/pma/services/indicadores", "ac4ef0ec195ed24ab08d1e4a8a3a1ed0");
+			"https://50.17.210.152/pma/services/indicadores",
+			"ac4ef0ec195ed24ab08d1e4a8a3a1ed0");
 
-	public static final ServicoPma SERVICO_PMA_LOCAL = new ServicoPma("http://localhost:3000/services/indicadores",
+	public static final ServicoPma SERVICO_PMA_LOCAL = new ServicoPma(
+			"http://localhost:3000/services/indicadores",
 			"ac4ef0ec195ed24ab08d1e4a8a3a1ed0");
 
 	public static final ServicoPma SERVICO_PMA = SERVICO_PMA_AMAZON;
 
 	public static JsonArray buscarDadosProjetos() {
 		LOG.info("Criando planilhaDexboard");
-		PlanilhaDexboard planilhaDexboard = new PlanilhaDexboard("0Au2Lk990DvFfdGVDQm9rTW1OYmw3dW5yOUVQSkdPSGc");
+		PlanilhaDexboard planilhaDexboard = new PlanilhaDexboard(
+				"0Au2Lk990DvFfdGVDQm9rTW1OYmw3dW5yOUVQSkdPSGc");
 		LOG.info("buscarDadosProjetos");
 		JsonArray dadosPlanilha = planilhaDexboard.buscarDadosDosProjetos();
 		LOG.info("criando json");
 		JsonArray obj = new JsonArray();
 		for (JsonElement elemento : dadosPlanilha) {
-			int idProjeto = Integer.parseInt(elemento.getAsJsonObject().get("id").getAsString());
+			int idProjeto = Integer.parseInt(elemento.getAsJsonObject()
+					.get("id").getAsString());
 			JsonObject dados = SERVICO_PMA.buscarDadosDoProjeto(idProjeto);
 			Utils.mesclar(dados, elemento.getAsJsonObject());
 			obj.add(dados);
@@ -47,7 +50,8 @@ public class Projeto {
 		TimeZone.setDefault(tz);
 		Calendar ca = GregorianCalendar.getInstance(tz);
 
-		SimpleDateFormat formatador = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		SimpleDateFormat formatador = new SimpleDateFormat(
+				"dd/MM/yyyy HH:mm:ss");
 		String modified = formatador.format(ca.getTime());
 		data.addProperty("lastModified", modified);
 
@@ -59,7 +63,8 @@ public class Projeto {
 	}
 
 	public static void limparCache() {
-		MemcacheService cache = MemcacheServiceFactory.getMemcacheService("dados-projetos");
+		MemcacheService cache = MemcacheServiceFactory
+				.getMemcacheService("dados-projetos");
 		cache.delete("dados-projetos");
 	}
 
