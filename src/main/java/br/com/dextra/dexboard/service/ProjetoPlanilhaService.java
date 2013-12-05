e7bf6e6f61e2d2a9fc1befb87ae9b7a6d35ee78a
Fernando Ultremare <feroult@gmail.com>
2013-12-05 09:16:53 -0200

    dead code

--- src/main/java/br/com/dextra/dexboard/service/ProjetoPlanilhaService.java
+++ src/main/java/br/com/dextra/dexboard/service/ProjetoPlanilhaService.java
@@ -10,36 +10,15 @@ import br.com.dextra.dexboard.planilha.PlanilhaPrincipal;
 
 import com.google.appengine.api.memcache.MemcacheService;
 import com.google.appengine.api.memcache.MemcacheServiceFactory;
-import com.google.gson.JsonObject;
 
 public class ProjetoPlanilhaService {
 
 	public final static Logger LOG = LoggerFactory
 			.getLogger(ProjetoPlanilhaService.class);
 
-	public static final PMAService SERVICO_PMA_AMAZON = new PMAService(
-			"https://pma.dextra.com.br/pma/services/indicadores",
-			"ac4ef0ec195ed24ab08d1e4a8a3a1ed0");
-
-	public static final PMAService SERVICO_PMA_LOCAL = new PMAService(
-			"http://localhost:3000/services/indicadores",
-			"ac4ef0ec195ed24ab08d1e4a8a3a1ed0");
-
-	public static final PMAService SERVICO_PMA = SERVICO_PMA_AMAZON;
-
 	public static Map<Long, Projeto> buscarDadosProjetosAtivos() {
 		PlanilhaPrincipal planilhaPrincipal = new PlanilhaPrincipal();
-
-		Map<Long, Projeto> projetos = planilhaPrincipal.buscarDadosDosProjetos();
-
-		/*
-		for (Projeto projeto : projetos.values()) {
-			JsonObject dados = SERVICO_PMA.buscarDadosDoProjeto(projeto
-					.getIdPma().intValue());
-			projeto.setCpi(dados.get("cpi").getAsDouble());
-		}*/
-
-		return projetos;
+		return planilhaPrincipal.buscarDadosDosProjetos();
 	}
 
 	public static void limparCache() {
