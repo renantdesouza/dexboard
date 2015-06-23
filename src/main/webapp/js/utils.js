b74182b47fec997e56b838d56e0dbdf36d199958
Walter Mesquita <wapmesquita@gmail.com>
2015-06-23 09:30:32 -0300

    alterando layout dexlife

---
+++ src/main/webapp/js/utils.js
@@ -0,0 +1,6 @@
+function getURLParameter(name) {
+	return decodeURIComponent((new RegExp('[?|&]' + name + '='
+			+ '([^&;]+?)(&|#|;|$)').exec(location.search) || [ , "" ])[1]
+			.replace(/\+/g, '%20'))
+			|| null;
+}
