61ff90a8a8a7e15fabcf24295e41608ee75a0cfa
Leonardo Couto <leonardo.couto@dextra-sw.com>
2016-05-18 15:41:06 -0300

    setando enconding da resposta

--- src/main/java/br/com/dextra/dexboard/servlet/QueryServlet.java
+++ src/main/java/br/com/dextra/dexboard/servlet/QueryServlet.java
@@ -28,6 +28,7 @@ public class QueryServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setCharacterEncoding("UTF-8");
 		resp.setContentType("application/json");
 		resp.getWriter().print(getJsonProjetosWithCache(req.getParameter(EQUIPE_HTTP_PARAMETER)));
 	}
