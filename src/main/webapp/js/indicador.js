2ca1c8247d79755dbe79f3bbeee95ccf0d1b274a
Leonardo Couto <leonardo.couto@gmail.com>
2016-05-22 14:57:58 -0300

    logica do indicador em um arquivo separado

---
+++ src/main/webapp/js/indicador.js
@@ -0,0 +1,196 @@
+var dexboard = window.dexboard || {};
+
+dexboard.indicador = (function($, Handlebars) {
+	
+	var dialog = {};
+	var service = {};
+	var template = null;
+	var view = {};
+	
+	var stopPropagation = function(event) {
+		if (event) {
+			event.preventDefault();
+			event.stopPropagation(); 
+		}
+		return false;
+	};
+	
+	dialog.Buttons = function(indicador) {
+		
+		var self = this;
+		var hasRecords = indicador.registros ? indicador.registros.length > 0 : false;
+		var textArea = new dialog.TextArea();
+		
+		this.change = $("#dialog input.change");
+		this.keep = $("#dialog input.btn-sem-mudancas");
+		
+		var enableChange = function() {
+			self.change.prop("disabled", false);
+			self.keep.prop("disabled", true);
+		};
+		
+		var disableChange = function() {
+			self.change.prop("disabled", true);
+			if (hasRecords) {
+				self.keep.prop("disabled", false);
+			}
+		};
+		
+		this.toggle = function(on) {
+			if (on) {
+				enableChange();
+			} else {
+				disableChange();
+			}
+		};
+		
+		this.init = function() {
+			
+			self.change.click(function(e) {
+				var button = $(this);
+				
+				if (!button.prop("disabled")) {
+					var comentario = textArea.val();
+					var status = button.prop("name");
+					console.info(status, comentario);
+				}
+			});
+			
+			self.keep.click(function(e) {
+				var button = $(this);
+				
+				if (!button.prop("disabled")) {
+					var ultimo = indicador.registros[0];
+					var comentario = ultimo.comentario;
+					var status = ultimo.classificacao;
+					console.info(status, comentario);
+					// TODO enviar um evento com a resposta quando o status for atualizado
+				}
+			});
+			
+			return self;
+		};
+	};
+	
+	dialog.Records = function() {
+		
+		var self = this;
+		
+		this.records = $("#dialog ul.historico");
+		this.title = $(".toggle-historico");
+		this.toggleChar = this.title.find("span");
+		
+		var isVisible = function() {
+			return self.records.is(":visible");
+		};
+		
+		var toggle = function(on) {
+			if (on) {
+				self.records.show();
+				self.toggleChar.html("-");
+			} else {
+				self.records.hide();
+				self.toggleChar.html("+");
+			}
+		};
+		
+		this.init = function() {
+			self.title.mousedown(stopPropagation);
+			
+			self.title.click(function(e) {
+				stopPropagation(e);
+				toggle(!isVisible());
+			});
+			
+			return self;
+		};
+	};
+	
+	dialog.TextArea = function() {
+		
+		var self = this;
+		
+		this.el = $("#dialog textarea");
+		
+		this.isEmpty = function() {
+			return self.el.val().length === 0; 
+		};
+		
+		this.val = function() {
+			return self.el.val();
+		};
+		
+		this.init = function(buttons) {
+			self.el.keyup(function() {
+				buttons.toggle(!self.isEmpty());
+			});
+			
+			return self;
+		};
+		
+	};
+	
+	view.Dialog = function() {
+		
+		var self = this;
+		
+		this.container = $("#dialog");
+		this.overlay = $("#dialog-overlay");
+		
+		this.close = function(event) {
+			stopPropagation(event);
+			self.overlay.hide();
+			self.container.dialog("close");
+		};
+		
+		this.open = function(projeto, indicador) {
+			
+			self.container.html(template({
+				"projeto" : projeto,
+				"indicador" : indicador
+			}));
+			
+			self.overlay.show();
+			self.container.dialog("open");
+			
+			self.container.find(".dialog-close-button").click(self.close);
+			
+			var buttons = (new dialog.Buttons(indicador)).init();
+			(new dialog.TextArea()).init(buttons);
+			(new dialog.Records()).init();
+			
+			return dialog;
+		};
+		
+		this.init = function() {
+			
+			self.overlay.click(self.close);
+			
+			$(document).keydown(function(e) {
+				if (e.which === 27) {
+					self.close(e);
+				}
+			});
+			
+			self.container.dialog({
+				"dialogClass": "no-close",
+				"autoOpen" : false,
+				"width" : 600
+			});
+
+		};
+		
+	};
+	
+	view.init = function() {
+		var source = $("#edicao-indicador-template").html();
+		template = Handlebars.compile(source);
+		(new view.Dialog()).init();
+	};
+
+	return {
+		"view" : view,
+		"service" : service
+	};
+	
+})(jQuery, Handlebars);
