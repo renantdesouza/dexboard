f287fa5ac51e7d26a013af1e5ca89639c7929bcc
renan.souza <r34573773>
2017-03-14 13:11:39 -0300

    alterações

--- src/main/java/br/com/dextra/dexboard/planilha/PlanilhaDexboard.java
+++ src/main/java/br/com/dextra/dexboard/planilha/PlanilhaDexboard.java
@@ -6,9 +6,11 @@ class PlanilhaDexboard extends Planilha {
 	
 	// TODO: trazer de propriedades do sistema
 
-	private static final String CHAVE_PLANILHA_DEXBOARD_PRODUCAO = "1Fjul3zOetgENCTfnnd3gDaxOEh3WaIQr4uV3Th2wRQo";
+	//private static final String CHAVE_PLANILHA_DEXBOARD_PRODUCAO = "1Fjul3zOetgENCTfnnd3gDaxOEh3WaIQr4uV3Th2wRQo";
 
 	private static final String CHAVE_PLANILHA_DEXBOARD_TESTE = "11z7PwH4sADaiXxshNPYr-88u3flqewCcGRGJok9cl6o";
+
+	private static final String CHAVE_PLANILHA_DEXBOARD_PRODUCAO = "11z7PwH4sADaiXxshNPYr-88u3flqewCcGRGJok9cl6o";
 
 	public PlanilhaDexboard(String nomePlanilha) {
 		super(getChavePlanilha(), nomePlanilha);
